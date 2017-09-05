401649398bb0b35d84691851ff109da358dc63ec
Alexander <alexandr290497@gmail.com>
2017-09-05 23:55:45 +0300

    FirstProgress2

---
+++ src/BarChart01.java
@@ -0,0 +1,30 @@
+import org.knowm.xchart.CategoryChart;
+        import org.knowm.xchart.CategoryChartBuilder;
+        import org.knowm.xchart.SwingWrapper;
+        import org.knowm.xchart.demo.charts.ExampleChart;
+        import org.knowm.xchart.style.Styler;
+
+
+class BarChart01 implements ExampleChart<CategoryChart> {
+    Main main = new Main();
+
+    public void showChart() {
+
+        ExampleChart<CategoryChart> exampleChart = new BarChart01();
+        CategoryChart chart = exampleChart.getChart();
+        new SwingWrapper<>(chart).displayChart();
+    }
+
+    @Override
+    public CategoryChart getChart() {
+
+        CategoryChart chart = new CategoryChartBuilder().width(800).height(600).title("Score Histogram").xAxisTitle("Score").yAxisTitle("Number").build();
+
+        chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideNW);
+        chart.getStyler().setHasAnnotations(true);
+
+        chart.addSeries("test 1",main.getMyArr(),main.getMyArr2());
+
+        return chart;
+    }
+}
